6e06d4639b1d8cfad0fd6d92e552a6446bc68e82
Marina Barros <mob@cin.ufpe.br>
2017-12-05 00:13:21 -0300

    [FINISHES #153339348] implementação de funcionalidade

--- server/repositories/course.ts
+++ server/repositories/course.ts
@@ -4,6 +4,12 @@ export class CourseRepository {
   courses: Course[] = [];
 
   insert(course: Course): Course {
+  	for (var i = 0; i < this.courses.length; ++i) {
+  		if (course.name === this.courses[i].name) {
+  			return null;
+  		}
+  	}
+  	
     this.courses.push(course);
     return course;
   }
